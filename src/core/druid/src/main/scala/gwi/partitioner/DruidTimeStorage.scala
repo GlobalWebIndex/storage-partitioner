c9f6021e0c61eb612b96b3c97e803f04cf61247b
l15k4 <liska.jakub@gmail.com>
2017-04-12 12:07:37 +0100

    DruidTimeStorage with new API

--- src/core/druid/src/main/scala/gwi/partitioner/DruidTimeStorage.scala
+++ src/core/druid/src/main/scala/gwi/partitioner/DruidTimeStorage.scala
@@ -1,6 +1,7 @@
 package gwi.partitioner
 
 import gwi.druid.client.DruidClient
+import org.joda.time.chrono.ISOChronology
 import org.joda.time.{DateTime, DateTimeZone, Interval}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -9,15 +10,11 @@ import scala.language.implicitConversions
 
 case class DruidSource(dataSource: String, coordinator: String, overlord: String, broker: String, access: String, properties: Map[String,String]) extends StorageSource
 
-case class DruidTimeStorage(id: String, source: DruidSource, partitioner: IdentityTimePartitioner) extends TimeStorage[DruidSource, IdentityTimePartitioner, TimeClient] {
-  type OUT = IdentityPointer
-  def lift(p: TimePartition): IdentityPointer = IdentityPointer(p.value.toString)
-}
+case class DruidTimeStorage(id: String, source: DruidSource, partitioner: PlainTimePartitioner) extends TimeStorage[DruidSource, PlainTimePartitioner, TimeClient]
 
 object DruidTimeStorage {
   implicit class DruidTimeStoragePimp(underlying: DruidTimeStorage) {
     def client(implicit driver: DruidClient) = new TimeClient {
-      type OUT = IdentityPointer
       private val DruidTimeStorage(_, source, partitioner) = underlying
 
       def delete(partition: TimePartition): Unit = {} //deleting segments is not real-time, ie. delete & create of the same partition would not have deterministic outcome
@@ -32,7 +29,7 @@ object DruidTimeStorage {
           driver.forQueryingCoordinator(source.coordinator)(10.seconds, 1.minute)
             .listDataSourceIntervals(source.dataSource).get
             .getOrElse(Seq.empty)
-            .map(partitioner.buildPartition)
+            .map( i => underlying.partitioner.build(new Interval(i, ISOChronology.getInstanceUTC)) )
             .filter(p => range.contains(p.value))
             .sortWith { case (x, y) => x.value.getStart.compareTo(y.value.getStart) < 1 }
         }(ExecutionContext.Implicits.global)
