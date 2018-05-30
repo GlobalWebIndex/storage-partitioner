f47b5f4b1e74cda1012818de0650839d94ec0303
Michal Zaborec <michal.zabrec@gmail.com>
2018-05-30 10:33:03 +0200

    Ignore s3 client tests if a client configuration is missing.

--- src/core/gcs/src/test/scala/gwi/partitioner/gcs/AlpakkaS3ClientGcsWrapperSpec.scala
+++ src/core/gcs/src/test/scala/gwi/partitioner/gcs/AlpakkaS3ClientGcsWrapperSpec.scala
@@ -10,16 +10,27 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class AlpakkaS3ClientGcsWrapperSpec extends S3ClientSpec with AkkaSupport {
+  import AlpakkaS3ClientGcsWrapperSpec._
 
-  private[this] val gcsClient = {
-    val path = Paths.get("/home/pribor/tmp/storage-partitioner-test/key.json")
+  private[this] lazy val gcsClient = {
+    val path = Paths.get(getKeyPath.getOrElse(throw new Exception("No key found!")))
     val config = GoogleAuthConfiguration(path)
     GoogleCloudStorageClient(config)
   }
+
   override protected[this] def s3Client: S3Client = AlpakkaGcsS3ClientWrapper(gcsClient)
+
   override protected[this] def bucket: String = "gwiq-storage-partitioner-test"
 
   override protected def beforeAll(): Unit = try super.beforeAll() finally {
     Await.ready(gcsClient.deleteFolder(bucket, ""), 10.seconds)
   }
+
+  override protected[this] def ignore: Boolean = getKeyPath.isEmpty
+
+  private[this] def getKeyPath = sys.env.get(googleAppCredetials)
+}
+
+object AlpakkaS3ClientGcsWrapperSpec {
+  val googleAppCredetials = "GOOGLE_APPLICATION_CREDENTIALS"
 }
