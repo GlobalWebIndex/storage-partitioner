660646a52c0aa72684d6fb82a8e2be84df3d97ad
Michal Zaborec <michal.zabrec@gmail.com>
2018-05-31 17:38:00 +0200

    Read gcs client credentials from GOOGLE_APPLICATION_CREDENTIALS env by default.

--- src/core/gcs/src/test/scala/gwi/partitioner/gcs/AlpakkaS3ClientGcsWrapperSpec.scala
+++ src/core/gcs/src/test/scala/gwi/partitioner/gcs/AlpakkaS3ClientGcsWrapperSpec.scala
@@ -1,36 +1,22 @@
 package gwi.partitioner.gcs
 
-import java.nio.file.Paths
-
-import akka.stream.alpakka.googlecloud.storage.GoogleAuthConfiguration
-import akka.stream.alpakka.googlecloud.storage.impl.GoogleCloudStorageClient
 import gwi.partitioner.{AkkaSupport, S3Client, S3ClientSpec}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class AlpakkaS3ClientGcsWrapperSpec extends S3ClientSpec with AkkaSupport {
-  import AlpakkaS3ClientGcsWrapperSpec._
 
-  private[this] lazy val gcsClient = {
-    val path = Paths.get(getKeyPath.getOrElse(throw new Exception("No key found!")))
-    val config = GoogleAuthConfiguration(path)
-    GoogleCloudStorageClient(config)
-  }
-
-  override protected[this] def s3Client: S3Client = AlpakkaGcsS3ClientWrapper(gcsClient)
+  override protected[this] def s3Client: S3Client = AlpakkaGcsS3ClientWrapper()
 
   override protected[this] def bucket: String = "gwiq-storage-partitioner-test"
 
   override protected def beforeAll(): Unit = try super.beforeAll() finally {
-    Await.ready(gcsClient.deleteFolder(bucket, ""), 10.seconds)
+    val res = s3Client.listBucket(bucket).runForeach { o =>
+      s3Client.deleteObject(o.bucket, o.key)
+    }
+    Await.ready(res, 10.seconds)
   }
 
-  override protected[this] def ignore: Boolean = getKeyPath.isEmpty
-
-  private[this] def getKeyPath = sys.env.get(googleAppCredetials)
-}
-
-object AlpakkaS3ClientGcsWrapperSpec {
-  val googleAppCredetials = "GOOGLE_APPLICATION_CREDENTIALS"
+  override protected[this] def ignore: Boolean = AlpakkaGcsS3ClientWrapper.credentialsPath.isEmpty
 }
